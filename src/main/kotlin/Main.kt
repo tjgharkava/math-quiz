24d45e884b6ab0940c995c7c8b793a49c49a9a28
Temo <tdzhgarkava@yahoo.com>
2025-07-30 21:07:22 +0400

    answers commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,9 +12,37 @@ fun main () {
     var studentAnswer2 = Integer.valueOf(readLine())
 
     println("Name the type of triangle that has two equal sides:")
-    var studentAnswer3 = readLine()
+    val studentAnswer3 = readLine()
 
     println("What is value of 8* 6 - (3 - 2)")
     var studentAnswer4 = Integer.valueOf(readLine())
 
+    var points = 0
+
+    if (studentAnswer1 == quizAnswer1) {
+        points += 25
+    }
+
+    if (studentAnswer2 == quizAnswer2) {
+        points += 25
+    } else if (studentAnswer2 == quizAnswer2++ || studentAnswer2 == quizAnswer2--) {
+        points += 20
+        println("The answer $studentAnswer2 is within 1 point of $quizAnswer2")
+    }
+
+    if (studentAnswer3 == quizAnswer3) {
+        points += 25
+    } else if (studentAnswer3 == "equilateral") {
+        points += 10
+    } else {
+        points += 1
+    }
+
+    if (studentAnswer4 == quizAnswer4) {
+        points += 25
+    } else if (studentAnswer4 in 44..54) {
+        points += 20
+    } else {
+        points += 1
+    }
 }
