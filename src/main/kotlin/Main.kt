1f9ecd27c290a18e26af1e3da291f5aeaff2d355
Temo <tdzhgarkava@yahoo.com>
2025-07-30 20:57:51 +0400

    question commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,4 +5,16 @@ fun main () {
     val quizAnswer3 = "isosceles"
     val quizAnswer4 = 47
 
+    println("What is 75 / 5?")
+    var studentAnswer1 = Integer.valueOf(readLine())
+
+    println("6 * 2y = 36. What is value of y?")
+    var studentAnswer2 = Integer.valueOf(readLine())
+
+    println("Name the type of triangle that has two equal sides:")
+    var studentAnswer3 = readLine()
+
+    println("What is value of 8* 6 - (3 - 2)")
+    var studentAnswer4 = Integer.valueOf(readLine())
+
 }
